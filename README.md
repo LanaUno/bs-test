4b28a92c56642923e892ec083ef3c8ad3827645b
Svitlana Andrushenko <wwwli4e@gmail.com>
2025-04-30 16:00:51 +0300

    some changes

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # bs-test
-# bs-test
 ### Prepare and test
 
 ##### 1. Install Node version 20.15.0
@@ -15,8 +14,8 @@ cd ./bs-test
 ##### 5. To install all dependencies use "npm install"
 
 ##### 6. Repo has 3 config files. One - wdio.cong.js -  let run tests localy. In this case Android studio should be installed and open with running device into it that is going to be tested. Other configs let run tests on Browserstack app:
-- wdio.bs-gp3.conf.js - for Google Pixel 3 with android version 9.0
-- wdio.bs-op9.conf.js - for OnePlus 9 with android version 11.0
+##### - wdio.bs-gp3.conf.js - for Google Pixel 3 with android version 9.0
+##### - wdio.bs-op9.conf.js - for OnePlus 9 with android version 11.0
 
 ##### 7. To run tests on 'Google Pixel 3' type in "powershell" terminal:
 ```
--- wdio.bs-op9.conf.js
+++ wdio.bs-op9.conf.js
@@ -1,51 +1,47 @@
 exports.config = {
     user: process.env.BROWSERSTACK_USERNAME,
     key: process.env.BROWSERSTACK_ACCESS_KEY,
-
     updateJob: false,
-    specs: [
-      './test/specs/**.e2e.js'
-    ],
-
+    specs: ["./test/specs/**.e2e.js"],
     exclude: [],
-    hostname: 'hub.browserstack.com',
+    hostname: "hub.browserstack.com",
     services: [
-      [
-        'browserstack',
-        {
-          app: 'bs://b5627fd64c15f366e16b5648bef694d6f1241acd',
-          buildIdentifier: "Webdriverio Android",
-          browserstackLocal: true,
-          accessibility: false
-        },
-      ]
+        [
+            "browserstack",
+            {
+                app: "bs://b5627fd64c15f366e16b5648bef694d6f1241acd",
+                buildIdentifier: "Webdriverio Android",
+                browserstackLocal: true,
+                accessibility: false,
+            },
+        ],
     ],
     capabilities: [
-    {
-      'bstack:options': {
-        deviceName: 'OnePlus 9',
-        platformVersion: '11.0',
-        platformName: 'android',
-      }
-    }],
+        {
+            "bstack:options": {
+                deviceName: "OnePlus 9",
+                platformVersion: "11.0",
+                platformName: "android",
+            },
+        },
+    ],
     commonCapabilities: {
-      'bstack:options': {
-        projectName: "BrowserStack Sample",
-        buildName: "bstack-demo",
-        testObservability: true,
-        debug: true,
-        networkLogs: true,
-        percy: false,
-      }
+        "bstack:options": {
+            projectName: "BrowserStack Sample",
+            buildName: "bstack-demo",
+            testObservability: true,
+            debug: true,
+            networkLogs: true,
+            percy: false,
+        },
     },
     maxInstances: 1,
     waitforTimeout: 10000,
     connectionRetryTimeout: 90000,
     connectionRetryCount: 3,
-
-    framework: 'mocha',
+    framework: "mocha",
     mochaOpts: {
-      ui: 'bdd',
-      timeout: 20000
-    }
-}
+        ui: "bdd",
+        timeout: 20000,
+    },
+};
